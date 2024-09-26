389cb010503bf92d695eef918859fe2e5cdd8c8b
Vedika Patidar <vedikapatidar83197@gmail.com>
2024-09-26 14:39:00 +0530

    update

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var mongoose = require("mongoose")
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 const path = require('path');
 require('dotenv').config();
 
-const app = express()
+const app = express();
 const port = process.env.port;
 const uri = process.env.mongodb_uri;
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(express.static('dashboard'))
-app.use('/images', express.static('images'))
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.static('dashboard'));
+app.use('/images', express.static('images'));
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 
-// mongoose.connect('mongodb://localhost:27017/SportConnectData')
-// var db = mongoose.connection
-// db.on('error', () => console.log("Error in Connecting to Database"))
-// db.once('open', () => console.log("Connected to Database"))
-
-// MongoDB Atlas connection string
-const mongoURI = uri;
-
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-var db = mongoose.connection
-db.on('error', () => console.log("Error in Connecting to Database"))
-db.once('open', () => console.log("Connected to MongoDB Atlas"))
-
+// Updated MongoDB Atlas connection string
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  });
 
 // Sign Up Route
 app.post("/sign_up", (req, res) => {
-    var name = req.body.name
-    var email = req.body.email
-    var phno = req.body.phno
-    var username = req.body.username
-    var password = req.body.password
-    var role = req.body.role
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var username = req.body.username;
+    var password = req.body.password;
+    var role = req.body.role;
 
     var data = {
         "name": name,
@@ -49,71 +39,61 @@ app.post("/sign_up", (req, res) => {
         "phno": phno,
         "username": username,
         "password": password,
-        "role":role
-    }
+        "role": role
+    };
 
-    db.collection('users').insertOne(data, (err, collection) => {
+    mongoose.connection.collection('users').insertOne(data, (err, collection) => {
         if (err) {
-            throw err
+            throw err;
         }
-        console.log("Record Inserted Successfully")
-    })
+        console.log("Record Inserted Successfully");
+    });
+
     switch(role) {
         case 'admin':
-            return res.redirect('admindashboard.html')
+            return res.redirect('admindashboard.html');
         case 'coordinator':
-            return res.redirect('coordinatordashboard.html')
+            return res.redirect('coordinatordashboard.html');
         case 'instructor':
-            return res.redirect('instructordashboard.html')
+            return res.redirect('instructordashboard.html');
         case 'student':
-            return res.redirect('studentdashboard.html')
-        default: // For student or any other default role
-            return res.redirect('index.html')
+            return res.redirect('studentdashboard.html');
+        default:
+            return res.redirect('index.html');
     }
-})
+});
 
 // Login Route
 app.post("/login", (req, res) => {
-    var username = req.body.username
-    var password = req.body.password
-    var role = req.body.role
+    var username = req.body.username;
+    var password = req.body.password;
+    var role = req.body.role;
 
-    db.collection('users').findOne({ "username": username, "password": password, "role":role }, (err, user) => {
+    mongoose.connection.collection('users').findOne({ "username": username, "password": password, "role": role }, (err, user) => {
         if (err) {
-            throw err
+            throw err;
         }
         if (!user) {
-            console.log("Login Failed: User not found or wrong credentials")
-            return res.redirect('login_failed.html') // Redirect to a failure page
+            console.log("Login Failed: User not found or wrong credentials");
+            return res.redirect('login_failed.html'); // Redirect to a failure page
         } else {
-            console.log("Login Successful")
-            // Redirect to the dashboard or a successful login page
+            console.log("Login Successful");
 
             switch(user.role) {
                 case 'admin':
-                    return res.redirect('admindashboard.html')
+                    return res.redirect('admindashboard.html');
                 case 'coordinator':
-                    return res.redirect('coordinatordashboard.html')
+                    return res.redirect('coordinatordashboard.html');
                 case 'instructor':
-                    return res.redirect('instructordashboard.html')
+                    return res.redirect('instructordashboard.html');
                 case 'student':
-                    return res.redirect('studentdashboard.html')
-                default: // For student or any other default role
-                    return res.redirect('index.html')
+                    return res.redirect('studentdashboard.html');
+                default:
+                    return res.redirect('index.html');
             }
         }
-    })
-})
-
-// app.get("/", (req, res) => {
-//     res.set({
-//         "Allow-access-Allow-Origin": '*'
-//     })
-//     return res.redirect('index.html')
-// }).listen(3000);
-
-// console.log("Listening on port 3000")
-
+    });
+});
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname)));
